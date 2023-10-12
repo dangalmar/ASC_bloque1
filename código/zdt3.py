2290d31dd6ccbd19909f58c4c2d532b9a50a2ae9
Daniel Gallardo Martos <dangalmar@alum.us.es>
2023-10-12 17:18:57 +0200

    Corrección de errores zdt3

--- código/zdt3.py
+++ código/zdt3.py
@@ -3,7 +3,7 @@ import math
 def zdt3(individuo, dimensions):
     f_1 = individuo[0]
     g = 1 + (9 / (dimensions - 1)) * sum(individuo[1:])
-    h = 1 - (math.sqrt(f_1 / g) - (f_1 / g) * math.sin(10 * math.pi * f_1))
+    h = 1 - (math.sqrt(f_1 / g) + (f_1 / g) * math.sin(10 * math.pi * f_1))
     f_2 = g * h
 
     return f_1, f_2
